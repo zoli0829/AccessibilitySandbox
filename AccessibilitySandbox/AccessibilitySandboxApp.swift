6408a1d4aee152d918fa40cc4c5456cfdf81c60a
zoli0829 <zoli0829@gmail.com>
2025-05-12 12:02:38 +0100

    Initial commit

---
+++ AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Zoltan Vegh on 12/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AccessibilitySandbox/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AccessibilitySandbox
+//
+//  Created by Zoltan Vegh on 12/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
