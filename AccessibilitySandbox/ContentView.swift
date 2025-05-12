2a0c8366b9a7c3446024108143ecd2f595a2e12f
zoli0829 <zoli0829@gmail.com>
2025-05-12 14:14:28 +0100

    finished day 74

--- AccessibilitySandbox/ContentView.swift
+++ AccessibilitySandbox/ContentView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var value = 10
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Current value: \(value)")
+            
+            Button("Increment") {
+                value += 1
+            }
+            
+            Button("Decrement") {
+                value -= 1
+            }
         }
-        .padding()
+        .accessibilityElement()
+        .accessibilityLabel("Value")
+        .accessibilityValue(String(value))
+        .accessibilityAdjustableAction{direction in
+            switch direction {
+            case .increment:
+                self.value += 1
+            case .decrement:
+                self.value -= 1
+            @unknown default:
+                break
+            }
+        }
     }
 }
 
